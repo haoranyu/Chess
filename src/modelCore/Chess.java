be0f6d5ca726dd6b38c251aabfab024e12ded0b4
haoranyu <haoran_yu@hotmail.com>
2015-02-14 02:43:40 -0600

    Finish static GUI

--- src/modelCore/Chess.java
+++ src/modelCore/Chess.java
@@ -14,7 +14,7 @@ public class Chess {
 	 * @param args not specified
 	 */
 	public static void main(String[] args) {
-		new GUI(80);
+		new GUI(90);
 	}
 	
 }
--- src/views/GUI.java
+++ src/views/GUI.java
@@ -9,6 +9,7 @@ import java.awt.event.ActionListener;
 import javax.swing.*;
 
 import modelCore.ChessBoard;
+import modelCore.Position;
 /**
  * @author haoranyu
  *
@@ -29,6 +30,7 @@ public class GUI implements ActionListener {
 		
 		initializeFrame();
 		initializePanel();
+		initializePieces();
 		
 		this.gameFrame.setVisible(true);
 	}
@@ -37,6 +39,7 @@ public class GUI implements ActionListener {
 		this.gameFrame = new JFrame("Chess Game");
 		this.gameFrame.setSize(this.gameFrameWidth, this.gameFrameHeight);
 		this.gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
 	}
 	
 	private void initializePanel() {
@@ -50,7 +53,50 @@ public class GUI implements ActionListener {
                 g.drawImage(img, 0, 0, this.getWidth(), this.getHeight(), background.getImageObserver());  
             }  
         };  
-		this.gameFrame.add(this.gamePanel);
+        this.gameFrame.add(this.gamePanel);
+	}
+	
+	private void initializePieces() {
+		for(int row = this.chessBoard.row; row >= 1; row--) {
+			for(int col = 1; col <= this.chessBoard.col; col++) {
+				String type = chessBoard.getChessPieceInPosition(new Position(row,col)).getType();
+				String name = chessBoard.getChessPieceInPosition(new Position(row,col)).getName(); 
+				JButton piece = new JButton(getIconForPieces(type, name));
+				piece.setFont(new Font("Arial", Font.BOLD, this.gridSize / 2));
+				piece.setOpaque(false);
+				piece.setContentAreaFilled(false);
+				piece.setBorderPainted(false);
+				this.gamePanel.add(piece);
+			}
+		}
+	}
+	
+	private String getIconForPieces(String type, String name) {
+		String iconString = null;
+		if(type.equals("white")) {
+			switch(name) {
+			case "king": iconString = "\u2654"; break;
+			case "queen": iconString = "\u2655"; break;
+			case "rook": iconString = "\u2656"; break;
+			case "bishop": iconString = "\u2657"; break;
+			case "knight": iconString = "\u2658"; break;
+			case "pawn": iconString = "\u2659";
+			}
+		}
+		else if(type.equals("black")){ // black
+			switch(name) {
+			case "king": iconString = "\u265A"; break;
+			case "queen": iconString = "\u265B"; break;
+			case "rook": iconString = "\u265C"; break;
+			case "bishop": iconString = "\u265D"; break;
+			case "knight": iconString = "\u265E"; break;
+			case "pawn": iconString = "\u265F";
+			}
+		}
+		else {
+			iconString = "";
+		}
+		return iconString;
 	}
 	
 
