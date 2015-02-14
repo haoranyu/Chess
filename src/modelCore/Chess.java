4e8234789bc8c450aab70b824c54e1efdd72439c
haoranyu <haoran_yu@hotmail.com>
2015-02-14 01:50:37 -0600

    Added the background for chess panel

--- src/modelCore/Chess.java
+++ src/modelCore/Chess.java
@@ -14,7 +14,7 @@ public class Chess {
 	 * @param args not specified
 	 */
 	public static void main(String[] args) {
-		new GUI(100);
+		new GUI(80);
 	}
 	
 }
--- src/views/GUI.java
+++ src/views/GUI.java
@@ -16,29 +16,40 @@ import modelCore.ChessBoard;
 public class GUI implements ActionListener {
 	private ChessBoard chessBoard;
 	private JFrame gameFrame;
-	private JPanel gamePanel;
-	private JButton[][] board;
-	int gridSize;
+	private int gameFrameHeight;
+	private int gameFrameWidth;
+	private JPanel gamePanel; 
+	private int gridSize;
 	
 	public GUI(int size) {
 		this.gridSize = size;
 		this.chessBoard = new ChessBoard();
-
+		this.gameFrameHeight = this.chessBoard.row * this.gridSize;
+		this.gameFrameWidth = this.chessBoard.col * this.gridSize;
+		
 		initializeFrame();
 		initializePanel();
-
+		
+		this.gameFrame.setVisible(true);
 	}
 	
 	private void initializeFrame() {
 		this.gameFrame = new JFrame("Chess Game");
-		this.gameFrame.setSize(this.chessBoard.col * this.gridSize, this.chessBoard.row * this.gridSize);
+		this.gameFrame.setSize(this.gameFrameWidth, this.gameFrameHeight);
 		this.gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.gameFrame.setVisible(true);
 	}
 	
 	private void initializePanel() {
-		this.gamePanel = new JPanel(new GridLayout(this.chessBoard.col, this.chessBoard.row));
-		this.gamePanel.setBackground(Color.cyan);
+		this.gamePanel = new JPanel(new GridLayout(this.chessBoard.col, this.chessBoard.row)) {  
+			private static final long serialVersionUID = 1L;
+
+			@Override  
+            protected void paintComponent(Graphics g) {  
+            	ImageIcon background = new ImageIcon("img/chess_panel_bg.png");
+                Image img = background.getImage();  
+                g.drawImage(img, 0, 0, this.getWidth(), this.getHeight(), background.getImageObserver());  
+            }  
+        };  
 		this.gameFrame.add(this.gamePanel);
 	}
 	
